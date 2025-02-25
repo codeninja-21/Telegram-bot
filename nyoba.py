625157d3d262d27367313bedfda8028072bf8b26
Danishdazach <Dazach99@gmail.com>
2025-02-25 17:06:47 +0700

    Create nyoba.py

---
+++ nyoba.py
@@ -0,0 +1,27 @@
+# Nesting
+Capitals = {
+    "Frances": "Paris",
+    "Germay": "Berlin"
+}
+
+# Nesting a list in a Dictionary
+
+Travel_log= {
+    "Frances": {"Cities_Visited": ["Paris", "Little", "Dijon"], "Total visited": 12},
+    "Germany": {"Cities_Visited": ["Berlian", "Hamburger", "Stuttgart"], "Total_visits": 5},
+}
+
+# Nesting Dictionary in a List
+
+Travel_log= [
+    {
+    "Country" : "Frances", 
+    "Cities_Visited": ["Paris", "Little",
+    "Dijon"], "Total visited":12
+    },
+    {
+    "Country" : "Germany",
+    "Cities_Visited": ["Berlian", "Hamburger", "Stuttgart"],
+    "Total_visits": 5
+    },
+]
